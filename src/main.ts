351379c792b935c1fa4892a6de09bb410b706c3d
Jean <jean@JeanKei.local>
2025-05-28 12:12:35 +0800

    origin

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { RequestMethod } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  const rawOrigins = config.getOrThrow<string>("ALLOWED_ORIGIN");
+  const origins = rawOrigins.split(",").map((origin) => origin.trim());
 
   app.setGlobalPrefix("api", {
     exclude: [{ path: "auth/telegram/redirect", method: RequestMethod.GET }],
@@ -12,10 +16,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: [
-      "http://localhost:3000",
-      "https://charitable-revelation-multimedia-kept.trycloudflare.com/auth/login",
-    ],
+    origin: origins,
     credentials: true,
     exposedHeaders: "set-cookie",
   });
