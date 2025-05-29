6c127845318ac4879b85b0bb7233c690846491ed
Jean <jean@JeanKei.local>
2025-05-29 16:57:04 +0800

    add politics

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       "http://localhost:3000",
-      "https://submission-right-heaven-consistent.trycloudflare.com",
+      "https://advisors-motivated-repair-pens.trycloudflare.com",
       "https://uma.yi-wan.ru",
     ],
     credentials: true,
