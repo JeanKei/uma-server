3ba6da8ec32db9776485e54c411dda4b75d72480
Jean <jean@JeanKei.local>
2025-06-05 10:05:50 +0800

    add prisma schema

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       "http://localhost:3000",
-      "https://scope-rejected-presentations-jungle.trycloudflare.com",
+      "https://joins-the-citizens-europe.trycloudflare.com",
       "https://uma.yi-wan.ru",
     ],
     credentials: true,
